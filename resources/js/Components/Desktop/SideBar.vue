16bc89a20a0637e1bb0738f1332144474b662686
MetaMan13 <imamovica1397@gmail.com>
2021-10-27 15:19:19 +0200

    Desktop sidebar prototyping

--- resources/js/Components/Desktop/SideBar.vue
+++ resources/js/Components/Desktop/SideBar.vue
@@ -1,12 +1,44 @@
 <template>
-    <div class="hidden bg-red-200 lg:block lg:fixed lg:top-0 lg:w-52 lg:left-0 lg:min-w-min lg:h-screen lg:z-40 lg:pt-16 lg:px-4">
-        <!-- lg:w-3/12 xl:w-2/12 2xl:w-1/12 -->
-        <p>ajsdjajs</p>
+    <div v-bind:class="{ 'lg:w-52': sideBarMenuOpen, 'lg:w-24' : !sideBarMenuOpen }" class="hidden bg-red-200 lg:block lg:fixed lg:top-0 lg:left-0 lg:min-w-min lg:h-screen lg:z-40 lg:pt-16 lg:px-4">
+        <!-- Side Bar Display Toggle button -->
+        <!-- lg:w-52 -->
+        <div class="absolute -right-4 bg-yellow-600 py-2 px-2 rounded-full">
+            <template v-if="sideBarMenuOpen">
+                <arrow-left
+                    @click="sideBarMenuOpen = false"
+                    :height="20" 
+                    :width="20"
+                    class="text-white"
+                    >
+                </arrow-left>
+            </template>
+
+            <template v-else>
+                <arrow-left
+                    @click="sideBarMenuOpen = true"
+                    :height="20" 
+                    :width="20"
+                    class="text-white transform rotate-180"
+                    >
+                </arrow-left>
+            </template>
+        </div>
     </div>
 </template>
 
 <script>
+
+import ArrowLeft from '../Icons/ArrowLeft.vue'
+
 export default {
     name: 'SideBar',
+    components: {
+        ArrowLeft,
+    },
+    data(){
+        return{
+            sideBarMenuOpen: true,
+        }
+    },
 }
 </script>
