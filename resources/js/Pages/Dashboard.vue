bf51d5980c76f5d1d7789a6851fa7e19eb924c05
MetaMan13 <imamovica1397@gmail.com>
2021-10-31 14:29:59 +0100

    Dashboard index

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,54 +1,18 @@
 <template>
-
-    <!-- Root main layout wrapper -->
-    <div class="min-h-screen w-full bg-gray-50 text-gray-700 dark dark:bg-gray-900">
-
-        <!-- Navigation -->
-        <navigation></navigation>
-        
-        <!-- Mobile User Action Bar -->
-        <user-action-bar></user-action-bar>
-
-        <!-- Desktop Side Bar -->
-        <side-bar @sideBarOpen="sideBarOpen" @sideBarClosed="sideBarClosed"></side-bar>
-
-        <!-- Content START -->
-        <div v-bind:class="{'lg:ml-56 xl:ml-60': sideBarMenuOpen, 'lg:ml-24 xl:ml-24' : !sideBarMenuOpen }" class="pb-12 pt-12 lg:px-4 lg:pb-0 lg:pt-16 lg:pb-4">
-            <div class="w-full bg-white h-full">
-                <slot></slot>
-            </div>
-        </div>
-        <!-- Content END -->
-
+    <div>
+        <p>Dashboard INDEX</p>
     </div>
-    <!-- Root main layout wrapper -->
-
 </template>
 
 <script>
-import UserActionBar from '../Components/Mobile/UserActionBar.vue'
-import Navigation from '../Components/PageParts/Navigation.vue'
-import SideBar from '../Components/Desktop/SideBar.vue'
+
+import UserDashboard from './../Layouts/UserDashboard.vue'
 
 export default {
+    name: 'Dashboard',
+    layout: UserDashboard,
     components: {
-        Navigation,
-        UserActionBar,
-        SideBar,
+
     },
-    data(){
-        return{
-            hamburgerMenuOpen: false,
-            sideBarMenuOpen: true,
-        }
-    },
-    methods: {
-        sideBarOpen(){
-            this.sideBarMenuOpen = true
-        },
-        sideBarClosed(){
-            this.sideBarMenuOpen = false
-        }
-    }
 }
 </script>
