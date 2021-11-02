2d19fdc9eab9e4d5f8968bb7decd4620a0fb983a
MetaMan13 <imamovica1397@gmail.com>
2021-11-02 20:53:47 +0100

    Artisan 'storage:public-clear' command

---
+++ app/Console/Commands/ClearPublicStorage.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+
+class ClearPublicStorage extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'storage:public-clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete all files from storage/app/public';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // Confirm that the user wants to delete all files before deleting
+        if($this->confirm('Are you sure you want to delete all files from storage/app/public ?'))
+        {
+            $files = new Filesystem();
+            $files->cleanDirectory('storage/app/public');
+        }
+    }
+}
