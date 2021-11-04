f3bd96c4dfe93168e6a027fb4a52cff8b08357c2
MetaMan13 <imamovica1397@gmail.com>
2021-11-04 05:30:44 +0100

    Finished create form prototype

--- resources/js/Pages/Video/Create.vue
+++ resources/js/Pages/Video/Create.vue
@@ -28,7 +28,7 @@
                 <div class="mt-6">
                     <div class="flex flex-col">
                         <label class="mb-1 text-base">Video description</label>
-                        <textarea v-model="form.description" cols="30" rows="10" placeholder="Enter a video description ( optional )" class="resize-none border border-gray-200 rounded-md focus:ring-transpatent focus:ring-0 focus:border-gray-300 placeholder-gray-400"></textarea>
+                        <textarea v-model="form.description" cols="20" rows="7" placeholder="Enter a video description ( optional )" class="border border-gray-200 rounded-md focus:ring-transpatent focus:ring-0 focus:border-gray-300 placeholder-gray-400"></textarea>
                     </div>
 
                     <div v-if="errors.description" class="mt-2">
@@ -39,14 +39,14 @@
                 <!-- Video file -->
                 <!-- File upload component START -->
                 <div class="mt-6">
-                    <div class="flex flex-col cursor-pointer" @click="$refs.fileInput.click()">
-                        <label for="" class="mb-1 text-base">Video file</label>
-                        <input @input="form.file = $event.target.files[0]" ref="fileInput" type="file" accept="video/mp4,video/x-m4v,video/*" class="hidden">
-                        <div class="bg-white py-10 border border-gray-200 rounded-md hover:shadow-sm hover:bg-gray-50">
+                    <div class="flex flex-col">
+                        <label for="" class="mb-1 text-base" :class="{ 'hidden' : form.processing }">Video file</label>
+                        <input @input="appendFile($event)" id="fileInput" ref="fileInput" type="file" accept="video/mp4,video/x-m4v,video/*" class="hidden">
+                        <div class="bg-white border border-gray-200 rounded-md hover:shadow-sm hover:bg-gray-50">
                             <div class="flex justify-center">
 
                                 <!-- If file input is null -->
-                                <div>
+                                <div v-if="form.file == null" @click="openFileExplorer" class="py-10 w-full h-full cursor-pointer">
                                     <div class="flex justify-center">
                                         <upload
                                             :height="24"
@@ -60,18 +60,24 @@
                                 </div>
 
                                 <!-- If file is selected -->
-                                <div>
+                                <div v-if="form.file != null" :class="{ 'hidden' : form.processing }" class="py-6 w-full h-full">
 
-                                    <div>
-
+                                    <div class="flex justify-center">
+                                        <p>{{ form.file.name }}</p>
                                     </div>
 
-                                    <div>
-
+                                    <div class="flex justify-center mt-6">
+                                        <button @click="discartFile" :disabled="form.processing" :class="{'cursor-not-allowed' : form.processing }" class="bg-red-500 hover:bg-red-600 text-white px-6 py-2 cursor-pointer rounded-md font-semibold flex gap-1 items-center justify-center select-none">
+                                            Discart
+                                            <trash
+                                                :width="16"
+                                                :height="16"
+                                                class="text-white self-center mb-1"
+                                                >
+                                            </trash>
+                                        </button>
                                     </div>
-
                                 </div>
-
                             </div>
                         </div>
                     </div>
@@ -81,25 +87,20 @@
                     </div>
                 </div>
                 <!-- File upload component END -->
+                
 
                 <!-- Form submit -->
                 <div class="mt-8">
                     <div>
-                        <button :class="{'bg-gray-100 text-gray-400 cursor-not-allowed' : form.processing }" :disabled="form.processing" class="bg-gray-600 text-white border border-gray-200 w-full py-4 rounded-md hover:bg-gray-700">Upload Video</button>
-                        <!-- <button :class="{'hidden' : form.processing }" :disabled="form.processing" type="submit" class="bg-gray-600 text-white border border-gray-200 w-full py-4 rounded-md hover:bg-gray-700">Upload Video</button> -->
-                        <p v-if="form.progress" class="mt-2">
-                            Uploading {{ form.progress.percentage}}%
-                        </p>
-                        <button v-show="form.processing" type="button" class="bg-red-500 text-white border border-gray-200 w-full py-4 rounded-md mt-2">Cancel</button>
-                    </div>
-
-                    <div>
-                        <progress v-if="form.progress" :value="form.progress.percentage" max="100">
-                            {{ form.progress.percentage }}%
-                        </progress>
-                        <p v-if="form.progress">
-                            {{ form.progress.percentage}}%
-                        </p>
+                        <div v-if="form.progress" class="mt-2 bg-gray-600 rounded-md flex flex-col items-center py-6">
+                            <p class="text-gray-300 mb-4">Uploading {{ form.progress.percentage }}%</p>
+                            <!-- <p class="text-gray-300 mb-4">Uplaoding 84%</p> -->
+                            <img src="/images/icons/loading.png" alt="Spinner" class="h-8 w-8 animate-spin">
+                        </div>
+                        <button :class="{'bg-gray-600 text-gray-400 cursor-not-allowed hover:bg-gray-600 hidden' : form.processing, 'bg-gray-600 hover:bg-gray-700 text-white' : !form.processing }" :disabled="form.processing" class="text-white border border-gray-200 w-full py-4 rounded-md">Upload Video</button>
+                        <button v-show="form.processing" @click="form.cancel()" type="button" class="bg-red-500 hover:bg-red-600 text-white border border-gray-200 w-full py-4 rounded-md mt-2">
+                            Cancel
+                        </button>
                     </div>
                 </div>
 
@@ -114,12 +115,14 @@ import { useForm } from '@inertiajs/inertia-vue3'
 import UserDashboard from './../../Layouts/UserDashboard.vue'
 import Upload from './../../Components/Icons/Upload.vue'
 import Movie from './../../Components/Icons/Movie.vue'
+import Trash from './../../Components/Icons/Trash.vue'
 
 export default {
     layout: UserDashboard,
     components: {
         Upload,
         Movie,
+        Trash,
     },
     props: {
         errors: Object,
@@ -134,9 +137,21 @@ export default {
         }
     },
     methods:{
+        // URL.createObjectURL()
         storeVideo(){
             this.form.post('/dashboard/videos', this.form)
         },
+        
+        openFileExplorer(){
+            this.$refs.fileInput.click()
+        },
+        appendFile(event){
+            this.form.file = event.target.files[0]
+        },
+        discartFile(){
+            document.getElementById('fileInput').value = null;
+            this.form.file = null
+        }
     },
 }
 </script>
